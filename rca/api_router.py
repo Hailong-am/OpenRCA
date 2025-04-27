c4a642648b3ef33be5ef266df89439f0920c6610
Hailong Cui <ihailong@amazon.com>
2025-04-27 10:22:39 +0800

    add bedrock support
    Signed-off-by: Hailong Cui <ihailong@amazon.com>

--- rca/api_router.py
+++ rca/api_router.py
@@ -1,6 +1,10 @@
+import json
 import os
 import yaml
 import time
+
+from botocore.exceptions import ClientError
+
 
 def load_config(config_path="rca/api_config.yaml"):
     configs = dict(os.environ)
@@ -51,6 +55,51 @@ def Anthropic_chat_completion(messages, temperature):
         temperature=temperature
     ).content
 
+
+#  aws bedrock
+def AWS_bedrock_completion(messages, temperature):
+    import boto3
+    session = boto3.session.Session(profile_name=configs["AWS_PROFILE"])
+    client = session.client(
+        service_name='bedrock-runtime',
+        region_name='us-east-1'
+    )
+
+    bedrock_message = []
+    for msg in messages:
+        content = msg['content']
+        role = msg['role']
+        bedrock_message.append({
+            "role": 'assistant' if role == 'system' else role,
+            "content": [
+                {
+                    "type": "text",
+                    "text": content
+                }
+            ]
+        })
+
+    try:
+        response = client.invoke_model(
+            modelId=configs["MODEL"],
+            body=json.dumps({
+                "anthropic_version": "bedrock-2023-05-31",
+                "max_tokens": 10000,
+                "temperature": temperature,
+                "messages": bedrock_message
+            })
+        )
+        # Decode the response body.
+        model_response = json.loads(response["body"].read())
+
+        # Extract and print the response text.
+        response_text = model_response["content"][0]["text"]
+        return response_text
+    except (ClientError, Exception) as e:
+        print(f"ERROR: Can't invoke, Reason: {e}")
+        raise e
+
+
 # for 3-rd party API which is compatible with OpenAI API (with different 'API_BASE')
 def AI_chat_completion(messages, temperature):    
     from openai import OpenAI
@@ -65,7 +114,6 @@ def AI_chat_completion(messages, temperature):
     ).choices[0].message.content
 
 def get_chat_completion(messages, temperature=0.0):
-
     def send_request():
         if configs["SOURCE"] == "AI":
             return AI_chat_completion(messages, temperature)
@@ -75,6 +123,8 @@ def get_chat_completion(messages, temperature=0.0):
             return Google_chat_completion(messages, temperature)
         elif configs["SOURCE"] == "Anthropic":
             return Anthropic_chat_completion(messages, temperature)
+        elif configs["SOURCE"] == "Bedrock":
+            return AWS_bedrock_completion(messages, temperature)
         else:
             raise ValueError("Invalid SOURCE in api_config file.")
     
